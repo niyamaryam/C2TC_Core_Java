b40f3342c750be4ef59171029f5487b56be35bae
Niya <niya@192.168.0.9>
2023-08-28 06:24:15 -0700

    core java commit

--- C2TC/src/com/tnsif/core/FirstProgram.java
+++ C2TC/src/com/tnsif/core/FirstProgram.java
@@ -3,6 +3,17 @@ package com.tnsif.core;
 public class FirstProgram {
 
 	public static void main(String[] args) {
+		
+		byte age = 34;
+		int age2 = 56;
+		char ch = 'A';
+		float f1 = 5.6f;
+		double d1 = 5.66D;
+		short age3 = 58;
+		boolean a = true;
+		String str = "Maryam";
+		long ageDino = 676645343658987l;//valid values java accept long L, Double D 
+		System.out.print(str);
 		System.out.println("Welcome to tnsif !!!");
 
 	}
